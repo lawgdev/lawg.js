c8574de0124a0a2b085fbc1c2e9a38d7a379ae4b
heybereket <semagnbereket@gmail.com>
2023-07-16 09:43:22 -0400

    v1.0.9

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,22 +1,24 @@
 export type Tags = Readonly<{ [key: string]: string | number }>;
 
 export type Metadata = {
-  readonly ua?: string;
   readonly tags?: Tags;
-};
-
-export interface UpdateEvent {
-  id?: string;
-  title?: string;
-  description?: string;
-  emoji?: string;
-  timestamp?: string;
-  metadata?: Metadata;
-  notify?: boolean;
+  readonly ua?: string;
 }
 
 export interface CreateEvent {
   title: string;
+  description?: string;
+  emoji?: string;
+  timestamp?: string;
+  metadata?: Metadata;
+  notify?: boolean | string[];
+  expiration: string;
+  triggers: string[];
+}
+
+export interface UpdateEvent {
+  id?: string;
+  title?: string;
   description?: string;
   emoji?: string;
   timestamp?: string;
